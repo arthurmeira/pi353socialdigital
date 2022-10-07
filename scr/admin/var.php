ba85e76010d3a3f2eaf50376ace4bbf0ec05b66b
Arthur Meria <85205144+arthurmeira@users.noreply.github.com>
2022-10-07 17:55:54 -0300

    Delete scr/admin directory

--- scr/admin/var.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $name_user = $_POST['nome_user'];
-    $cpf_user = $_POST['cpf_user'];
-    $rg_user = $_POST['rg_user'];
-    $email_user = $_POST['email_user'];
-    $celular_user = $_POST['cel_user'];
-    $dtNasc_user = $_POST['dtNasc_user'];
-    $dtCad_user = $_POST['dtCad_user'];
